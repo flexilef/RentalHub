8f9a164b56c54292d0144d0f4b4f3ef44e90fc44
Felix Lee <Felix Lee>
2016-10-14 16:53:03 -0700

    Updated DB_HOST

--- application/config/config.php
+++ application/config/config.php
@@ -53,7 +53,7 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
  * This is the place where you define your database credentials, database type etc.
  */
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1:13306');
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'student_fchanlee');
 define('DB_USER', 'fchanlee');
 define('DB_PASS', 'mariposa123');
