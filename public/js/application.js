8823a51513b94fe35f48a1da3a4b0409fd4d0d53
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-21 16:09:20 -0700

    event bug fixed

--- public/js/application.js
+++ public/js/application.js
@@ -1,45 +1,42 @@
-$(document).ready(function(){
-    $(function() {
+$(function() {
 
-        // just a super-simple JS demo
+    // just a super-simple JS demo
 
-        var demoHeaderBox;
+    var demoHeaderBox;
 
-        // simple demo to show create something via javascript on the page
-        if ($('#javascript-header-demo-box').length !== 0) {
-            demoHeaderBox = $('#javascript-header-demo-box');
-            demoHeaderBox
-                .hide()
-                .text('Hello from JavaScript! This line has been added by public/js/application.js')
-                .css('color', 'green')
-                .fadeIn('slow');
-        }
+    // simple demo to show create something via javascript on the page
+    if ($('#javascript-header-demo-box').length !== 0) {
+    	demoHeaderBox = $('#javascript-header-demo-box');
+    	demoHeaderBox
+    		.hide()
+    		.text('Hello from JavaScript! This line has been added by public/js/application.js')
+    		.css('color', 'green')
+    		.fadeIn('slow');
+    }
 
-        // if #javascript-ajax-button exists
-        if ($('#javascript-ajax-button').length !== 0) {
+    // if #javascript-ajax-button exists
+    if ($('#javascript-ajax-button').length !== 0) {
 
-            $('#javascript-ajax-button').on('click', function(){
+        $('#javascript-ajax-button').on('click', function(){
 
-                // send an ajax-request to this URL: current-server.com/songs/ajaxGetStats
-                // "url" is defined in views/_templates/footer.php
-                $.ajax(url + "/songs/ajaxGetStats")
-                    .done(function(result) {
-                        // this will be executed if the ajax-call was successful
-                        // here we get the feedback from the ajax-call (result) and show it in #javascript-ajax-result-box
-                        $('#javascript-ajax-result-box').html(result);
-                    })
-                    .fail(function() {
-                        // this will be executed if the ajax-call had failed
-                    })
-                    .always(function() {
-                        // this will ALWAYS be executed, regardless if the ajax-call was success or not
-                    });
-            });
-        }
+            // send an ajax-request to this URL: current-server.com/songs/ajaxGetStats
+            // "url" is defined in views/_templates/footer.php
+            $.ajax(url + "/songs/ajaxGetStats")
+                .done(function(result) {
+                    // this will be executed if the ajax-call was successful
+                    // here we get the feedback from the ajax-call (result) and show it in #javascript-ajax-result-box
+                    $('#javascript-ajax-result-box').html(result);
+                })
+                .fail(function() {
+                    // this will be executed if the ajax-call had failed
+                })
+                .always(function() {
+                    // this will ALWAYS be executed, regardless if the ajax-call was success or not
+                });
+        });
+    }
 
-    });
-
-// Get the modal
+    // Get the modal
     var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
