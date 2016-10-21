fa737765eade8f324f0c30b880b4a57fb3344ed8
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-21 15:49:37 -0700

    js load bug fixed

--- public/js/application.js
+++ public/js/application.js
@@ -1,92 +1,94 @@
-$(function() {
+window.onload = function(){
+    $(function() {
 
-    // just a super-simple JS demo
+        // just a super-simple JS demo
 
-    var demoHeaderBox;
+        var demoHeaderBox;
 
-    // simple demo to show create something via javascript on the page
-    if ($('#javascript-header-demo-box').length !== 0) {
-    	demoHeaderBox = $('#javascript-header-demo-box');
-    	demoHeaderBox
-    		.hide()
-    		.text('Hello from JavaScript! This line has been added by public/js/application.js')
-    		.css('color', 'green')
-    		.fadeIn('slow');
-    }
+        // simple demo to show create something via javascript on the page
+        if ($('#javascript-header-demo-box').length !== 0) {
+            demoHeaderBox = $('#javascript-header-demo-box');
+            demoHeaderBox
+                .hide()
+                .text('Hello from JavaScript! This line has been added by public/js/application.js')
+                .css('color', 'green')
+                .fadeIn('slow');
+        }
 
-    // if #javascript-ajax-button exists
-    if ($('#javascript-ajax-button').length !== 0) {
+        // if #javascript-ajax-button exists
+        if ($('#javascript-ajax-button').length !== 0) {
 
-        $('#javascript-ajax-button').on('click', function(){
+            $('#javascript-ajax-button').on('click', function(){
 
-            // send an ajax-request to this URL: current-server.com/songs/ajaxGetStats
-            // "url" is defined in views/_templates/footer.php
-            $.ajax(url + "/songs/ajaxGetStats")
-                .done(function(result) {
-                    // this will be executed if the ajax-call was successful
-                    // here we get the feedback from the ajax-call (result) and show it in #javascript-ajax-result-box
-                    $('#javascript-ajax-result-box').html(result);
-                })
-                .fail(function() {
-                    // this will be executed if the ajax-call had failed
-                })
-                .always(function() {
-                    // this will ALWAYS be executed, regardless if the ajax-call was success or not
-                });
-        });
-    }
+                // send an ajax-request to this URL: current-server.com/songs/ajaxGetStats
+                // "url" is defined in views/_templates/footer.php
+                $.ajax(url + "/songs/ajaxGetStats")
+                    .done(function(result) {
+                        // this will be executed if the ajax-call was successful
+                        // here we get the feedback from the ajax-call (result) and show it in #javascript-ajax-result-box
+                        $('#javascript-ajax-result-box').html(result);
+                    })
+                    .fail(function() {
+                        // this will be executed if the ajax-call had failed
+                    })
+                    .always(function() {
+                        // this will ALWAYS be executed, regardless if the ajax-call was success or not
+                    });
+            });
+        }
 
-});
+    });
 
 // Get the modal
-var modal = document.getElementById('myModal');
+    var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var imgs = document.getElementsByClassName('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-var myFunction = function() {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-};
+    var imgs = document.getElementsByClassName('myImg');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var myFunction = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    };
 
-for (var i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener('click', myFunction, false);
-}
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].addEventListener('click', myFunction, false);
+    }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+    }
 };
-
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
