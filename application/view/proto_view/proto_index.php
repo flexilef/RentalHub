ba828801d5e84b6156236f888de81019dc632926
Felix Lee <Felix Lee>
2016-10-21 16:24:35 -0700

    Changed image folder path.

--- application/view/proto_view/proto_index.php
+++ application/view/proto_view/proto_index.php
@@ -5,7 +5,7 @@
 
     <?php foreach ($imageresults as $image) { ?>
     <div class="mySlides fade">
-        <img class="myImg" width="100%" height="200px" src="./../uploads/<?php echo $image->image_name?>">
+        <img class="myImg" width="100%" height="200px" src="<?php echo APP . "uploads/"; ?><!--./../uploads/--><?php echo $image->image_name?>">
     </div>
   <?php } ?>
 
