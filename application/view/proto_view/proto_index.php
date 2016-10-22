1ea57ea79a71b70e5a8fc78bfdc09bfff70fa8a7
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-22 11:55:11 -0700

    displaying images after posting only

--- application/view/proto_view/proto_index.php
+++ application/view/proto_view/proto_index.php
@@ -3,23 +3,22 @@
 ?>
 <div class="container">
 
-  <h3>Displaying images:</h3>
-  <div class="slideshow-container">
-
-    <?php foreach ($imageresults as $image) { ?>
-    <div class="mySlides fade">
-        <img class="myImg" width="100%" height="200px" src="./../uploads/<?php echo $image->image_name?>">
-    </div>
-  <?php } ?>
-
-    <a class="prev" onclick="plusSlides(-1)">❮</a>
-    <a class="next" onclick="plusSlides(1)">❯</a>
-
-  </div>
-  <br>
-  <?php $count = 1?>
-
   <?php if (isset($imageresults)) { ?>
+    <h3>Displaying images:</h3>
+    <div class="slideshow-container">
+
+      <?php foreach ($imageresults as $image) { ?>
+        <div class="mySlides fade">
+          <img class="myImg" width="100%" height="200px" src="./../uploads/<?php echo $image->image_name?>">
+        </div>
+      <?php } ?>
+
+      <a class="prev" onclick="plusSlides(-1)">❮</a>
+      <a class="next" onclick="plusSlides(1)">❯</a>
+
+    </div>
+    <br>
+    <?php $count = 1?>
   <div style="text-align:center">
   <?php foreach ($imageresults as $image) { ?>
       <span class="dot" onclick="currentSlide(<?php echo $count?>)"></span>
