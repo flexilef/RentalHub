9ef84119d4b7e38a18a90d0f62ec5da5db22f8cf
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-25 14:47:49 -0700

    image bug in search result fix

--- application/view/proto_view/proto_index.php
+++ application/view/proto_view/proto_index.php
@@ -3,8 +3,10 @@
       <h3>Rental Space Title</h3>
       <input type="text" name="rental_title"/>
       <h3>Upload Pictures</h3>
-      <input multiple="true" type="file" name="images[]" />
-      <br>
-      <input type="submit" name="submit_post" value="Submit" />
+      <div>
+          <input multiple="true" type="file" name="images[]" />
+          <br>
+          <input type="submit" name="submit_post" value="Post" />
+      </div>
     </form>
 </div>
--- application/view/view_search_results/index.php
+++ application/view/view_search_results/index.php
@@ -13,29 +13,31 @@ if(isset($rentalId_to_images)) { ?>
                 <?php echo "    "; ?>
             <?php } ?>
         <?php } ?>
-    <div id="myModal" class="modal">
-        <div class="slideshow-container">
-            <?php foreach ($rentalIds as $id) { ?>
-                <?php $images = $rentalId_to_images[$id]; ?>
-                <?php foreach($images as $image_name) { ?>
-                <div class="mySlides fade">
-                <img width="100%" height="600px" src="./../uploads/<?php echo $image_name; ?>">
+        <?php foreach ($rentalIds as $id) { ?>
+        <?php $images = $rentalId_to_images[$id]; ?>
+            <div id="myModal" class="modal">
+                <div class="slideshow-container">
+
+                        <?php foreach($images as $image_name) { ?>
+                        <div class="mySlides fade">
+                        <img width="100%" height="600px" src="./../uploads/<?php echo $image_name; ?>">
+                        </div>
+                        <?php } ?>
+                    <span class="close">×</span>
+                    <a class="prev" onclick="plusSlides(-1)">❮</a>
+                    <a class="next" onclick="plusSlides(1)">❯</a>
+                    <br>
+                    <?php $count = 1?>
+                    <div style="text-align:center">
+                        <?php foreach ($images as $image) { ?>
+                            <span class="dot" onclick="currentSlide(<?php echo $count?>)"></span>
+                            <?php $count += 1?>
+                        <?php } ?>
+                    </div>
                 </div>
-                <?php } ?>
-            <?php } ?>
-            <span class="close">×</span>
-            <a class="prev" onclick="plusSlides(-1)">❮</a>
-            <a class="next" onclick="plusSlides(1)">❯</a>
-            <br>
-            <?php $count = 1?>
-            <div style="text-align:center">
-                <?php foreach ($images as $image) { ?>
-                    <span class="dot" onclick="currentSlide(<?php echo $count?>)"></span>
-                    <?php $count += 1?>
-                <?php } ?>
             </div>
-        </div>
+        <?php } ?>
+
     </div>
-</div>
 
 <?php } ?>
