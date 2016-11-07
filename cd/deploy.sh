14fa504a2f38be4bdf1831688cdec2222d112394
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-11-06 18:31:17 -0800

    changed the branch in the script

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -3,5 +3,5 @@ echo "deploying to sfsuswe.com"
 
 ssh -v f16g16@sfsuswe.com <<f16g16
     cd ~/public_html/final_project
-    git pull gitlab view-single-posting
+    git pull gitlab view_single_posting
 f16g16
