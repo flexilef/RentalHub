c25c5bca63a46c97ecd206519f170e831dff8161
Steven Ryan Lum <slum@mail.sfsu.edu>
2016-11-08 20:47:12 -0800

    Added aim of website, disclaimer, and image of SF to home page

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,4 +1,18 @@
 <div class="container">
-    <h2>CSC 848/6484 Software Engineering</h2>
+    <h2>CSC 848/648 Software Engineering</h2>
     <h3>Section-2 Group 16</h3>
+    <h4 style="font-size: 20pt">
+        SFSU Rental Hub aims to provide an easy way for SFSU students to find 
+        available housing, posted by residents of the Bay Area.
+    </h4>
+    <h5 style="color: red; font-size: 20pt">
+        Disclaimer: SFSU/FAU/Fulda Software Engineering Project, Fall 2016.
+        For Demonstration Only
+    </h5>
+    <img src="public/img/San_Francisco_(Evening).jpg" alt="sf" style="width: 90%; height: 90%">  <!--https://commons.wikimedia.org/wiki/File:San_Francisco_(Evening).jpg-->
+    <div>
+        Source:
+        <a href="https://commons.wikimedia.org/wiki/File:San_Francisco_(Evening).jpg">Basil D Soufi, Wikimedia</a>
+    </div>
+    
 </div>
