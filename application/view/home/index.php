b3499044767fe26a82564fd3f7c45f56db92d56d
stevielum1 <stevielum1@gmail.com>
2016-11-12 19:02:20 -0800

    added cards of rental listings for the landing page

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -10,37 +10,70 @@
         SFSU Rental Hub aims to provide an easy way for SFSU students to find
         available housing, posted by residents of the Bay Area.
     </h4>
-    <div style="position: relative">
-        <table style="float: left; border-collapse: collapse; margin-left:25px; margin-right: 25px">
-            <td style="border: 1px solid black">
-                <p style="font-size: 12pt" align="center">RECOMMENDED LISTING 1</p>
-                <img src="public/uploads/14370-design-of-modern-apartment-free-desktop-wallpapers-download.jpg" alt="listing1" style="width:300px; height: 300px">
-            </td>
-            <tr><td style="border: 1px solid black">Title</td></tr>
-            <tr><td style="border: 1px solid black">Description</td></tr>
-            <tr><td style="border: 1px solid black; text-align: center"><button type="button">Rent</button></td></tr>
-        </table>
-    </div>
-    <div>
-        <table style="float: left; border-collapse: collapse; margin-left:25px; margin-right: 25px">
-            <td style="border: 1px solid black">
-                <p style="font-size: 12pt" align="center">RECOMMENDED LISTING 2</p>
-                <img src="public/uploads/31892-luxury_vacation_apartment-t2.jpg" alt="listing2" style="width:300px; height: 300px">
-            </td>
-            <tr><td style="border: 1px solid black">Title</td></tr>
-            <tr><td style="border: 1px solid black">Description</td></tr>
-            <tr><td style="border: 1px solid black; text-align: center"><button type="button">Rent</button></td></tr>
-        </table>
-    </div>
-    <div>
-        <table style="float: left; border-collapse: collapse; margin-left:25px; margin-right: 25px">
-            <td style="border: 1px solid black">
-                <p style="font-size: 12pt; "align="center">RECOMMENDED LISTING 3</p>
-                <img src="public/uploads/57821-furniture_sofa_interior_design_style_comfort_68132_1920x1080.jpg" alt="listing3" style="width:300px; height: 300px">
-            </td>
-            <tr><td style="border: 1px solid black">Title</td></tr>
-            <tr><td style="border: 1px solid black">Description</td></tr>
-            <tr><td style="border: 1px solid black; text-align: center"><button type="button">Rent</button></td></tr>
-        </table>
-    </div>
+	<br><br>
+	<div class="row" style="display: flex; justify-content: space-around; align-items: center">
+        <div class="col-xs-18 col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="public/uploads/14370-design-of-modern-apartment-free-desktop-wallpapers-download.jpg" class="imgsize">
+				<div class="caption">
+					<h4 class="title">
+						<?php
+							$string = 'big room';
+							echo $string;
+						?>
+					</h4>
+					<p class="desc">
+						<?php
+							$string = 'asdf wefawef lkn dcadc iaflnsf licn ianskn inasfinsaf iini cqppe cidv iqwqcncq ncn inasicnasc inv nevu nevunev asimf ansf siijq pkqpk ;lasnfn kasfknkn ijc iqfk';
+							echo $string;
+						?>
+					</p>
+					<a href="#" class="btn btn-info btn-lg" role="button">View</a>
+					<a href="#" class="btn btn-default btn-lg pull-right" role="button">Rent</a>
+				</div>
+			</div>
+        </div>
+		<div class="col-xs-18 col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="public/uploads/31892-luxury_vacation_apartment-t2.jpg" class="imgsize">
+				<div class="caption">
+					<h4 class="title">
+						<?php
+							$string = 'apartment near sfsu';
+							echo $string;
+						?>
+					</h4>
+					<p class="desc">
+						<?php
+							$string = 'kaus asdpokq od osnc isinsf insn insininsc ubf wdakv aisnc ir ivbwinfc isc inscbw jnasjn nso asnvwi ucb uuasjcbasjcb kaus uahsdb iasdasd inw omdnjdvna okcmo ojcjndan iajsnasf kasndkn';
+							echo $string;
+						?>
+					</p>
+					<a href="#" class="btn btn-info btn-lg" role="button">View</a>
+					<a href="#" class="btn btn-default btn-lg pull-right" role="button">Rent</a>
+				</div>
+			</div>
+        </div>
+		<div class="col-xs-18 col-sm-6 col-md-3">
+			<div class="thumbnail">
+				<img src="public/uploads/57821-furniture_sofa_interior_design_style_comfort_68132_1920x1080.jpg" class="imgsize">
+				<div class="caption">
+					<h4 class="title">
+						<?php
+							$string = 'super awesome apartment with super awesome neighbors';
+							echo $string;
+						?>
+					</h4>
+					<p class="desc">
+						<?php
+							$string = 'big room one bedroom one closet one desk super cool dogs and cats allowed close to san francisco state university only takes 15 minutes to get to campus';
+							echo $string;
+						?>
+					</p>
+					<a href="#" class="btn btn-info btn-lg" role="button">View</a>
+					<a href="#" class="btn btn-default btn-lg pull-right" role="button">Rent</a>
+				</div>
+			</div>
+        </div>
+	</div>
 </div>
