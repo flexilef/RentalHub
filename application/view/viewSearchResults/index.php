8cf6ba7bd9b198f90714eac090155bad2e5b5508
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-11-14 22:09:32 -0800

    result is filtering for sorting

--- application/view/viewSearchResults/index.php
+++ application/view/viewSearchResults/index.php
@@ -51,7 +51,7 @@
                             </select>
                         </div>
                     </div>
-                    <p class="text-center"><a href="#" class="btn btn-danger glyphicon glyphicon-search" role="button"></a></p>
+                    <p class="text-center"><a id="filter" href="<?php echo  URL . 'searchResults/index?' ?>" class="btn btn-danger glyphicon glyphicon-search" role="button"></a></p>
                 </form>
             </div>
         </div>
