8be2d95c982c6051001244541fdb144539099531
Muhammad Osama Nasir <muhammad-osama.nasir@informatik.hs-fulda.de>
2016-11-30 01:59:30 +0000

    Add Logout Functionality In Controller

--- application/controller/sprofile.php
+++ application/controller/sprofile.php
@@ -12,7 +12,12 @@ class Sprofile extends Controller {
 
     public function index() {
         //Execute on Registeration @ActionEvent
+        //Validation of the fields must be done via javascript
+        // so that errors or missing field option can be shown on
+        // the same view.It will prevent extra round trip.
+
         if (isset($_POST['register'])) {
+
             $status = false; //Query Status 
             $fname = $_POST['fname'];
             $email = $_POST['email'];
@@ -29,50 +34,59 @@ class Sprofile extends Controller {
                 $landlord = $_POST['landLord'];
             }
 
-            $status = $this->signin_model->registerUser($fname, $email, $password, $verify, $student, $landlord);
+            // create an MD5 hash of the password
+            $password = md5($password);
+
+            $status = $this->signin_model->registerUser($fname, $email, $password, $student, $landlord);
 
             if ($status == true) {
                 //Register Successfully ,Set the $fname in Session variable
                 $this->getSessionID($email, $password);
             } else {
-                //Show Warning to User
+                //Some PopUp Should be called here to display error message
                 echo 'Registeration Failed,Please Contact Administrator';
             }
-            
+
             require APP . "view/signinfolder/index.php";
         }
 
         //Execute on SignIn @ActionEvent
         if (isset($_POST['sign-in']) && (!empty($_POST['email']) && !empty($_POST['password']))) {
-            
+
             $email = $_POST['email'];
             $password = $_POST['password'];
+
+            // create an MD5 hash of the password
+            $password = md5($password);
+
             $fname = $_POST['fname'];
             $this->getSessionID($email, $password);
-        
         }
-   
-        //This is hardCoded,Will be change in Future
-        header('Location: http://localhost/Property/index.php');
-//        require APP . 'view/_templates/header.php';
-//        require APP . 'view/_templates/footer.php';
+
+        // Once the sessions variables have been set, redirect them to the landing page / home page.
+        header('Location: ' . URL);
     }
 
     //Get the Session ID 
-    //Function allows perform Login Operation
     public function getSessionID($email, $password) {
         $userdetail = $this->signin_model->signIn($email, $password);
         if (!empty($userdetail)) {
-            $_SESSION['name'] = $userdetail[0]['FULL_NAME'];
-            $_SESSION['id'] = $userdetail[0]['ID'];
+            // is_auth is important here because we will test this to make sure they can view other pages
+            // that are needing credentials.
+            $_SESSION['is_auth'] = true;
+            $_SESSION['name'] = $userdetail[0]['FULL_NAME']; //Shown on welcome
+            $_SESSION['id'] = $userdetail[0]['ID']; //Use to record transactions
         }
     }
 
     public function logout() {
         unset($_SESSION['name']);
+        unset($_SESSION['is_auth']);
+        unset($_SESSION['id']);
         session_destroy();
+
+        header('Location: ' . URL);
         //header("Location: ".$_SERVER['REQUEST_URI']);
-        //exit;
     }
 
 }
