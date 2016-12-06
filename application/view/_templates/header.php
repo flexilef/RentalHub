3b6298733f2453dbb1feb241871cccfd12e1dc4a
Felix Lee <Felix Lee>
2016-12-06 01:17:08 -0800

    Added input validation for sign up and sign in. Layout of radio boxes in sign up is still broken.

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -8,12 +8,19 @@
 
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+        
+        <!-- JQuery -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+        
+        <!-- Bootstrap Validator -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.7/validator.js" integrity="sha256-ZrJ1ZbedU+b0jUh9nHnV3GagEOUeckZ2DY0BV0lF3pg=" crossorigin="anonymous"></script>
+        
         <!-- CSS -->
         <link href="<?php echo URL; ?>css/style.less" rel="stylesheet/less">
 
         <!-- Extra icons -->
         <script src="https://use.fontawesome.com/c8d876cc7d.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
     </head>
     <header>
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -38,19 +45,52 @@
             </div>
             <div class="collapse navbar-collapse pull-right">
                 <ul class="nav navbar-nav">
-                    <li><a href="<?php echo URL; ?>protoController/index">Post a Listing</a></li>
+                    <?php
+                    if (isset($_SESSION['is_auth'])) {
+                        ?>
+                        <li><a href="<?php echo URL; ?>protoController/index">Post a Listing</a></li>
+                        <?php
+                    }else {
+                        ?>
+                        <li><a href="#" type="button" data-toggle="modal" data-target="#sign-in-modal">Post a Listing</a></li>
+                        <?php
+                    }
+                    ?>
                     <li><a href="<?php echo URL; ?>profile/index">Profile & Postings</a></li>
-                    <li><a href="#" type="button" data-toggle="modal" data-target="#sign-up-modal">
-                            Sign Up
-                        </a></li>
-                    <li><a href="#" type="button" data-toggle="modal" data-target="#sign-in-modal">
-                            <?php
-                            if (isset($_SESSION['name'])) {
-                                echo "Welcome " . $_SESSION['name'];
-                            } else {
-                                echo "Sign In";
+                      <?php 
+                            if (!isset($_SESSION['is_auth'])) {?>
+                            <li><a href="#" type="button" data-toggle="modal" data-target="#sign-up-modal">
+                             <?php echo "Sign Up";
                             }
-                            ?></a></li>
+                            ?>
+                        </a>
+                    </li>
+                    
+                     <?php
+                            if (!isset($_SESSION['is_auth'])) {?>
+                             <li><a href="#" type="button" data-toggle="modal" data-target="#sign-in-modal">
+                              <?php   echo "Sign In";
+                            }
+                            ?></a>
+                    </li>
+                    <li> <a href="#" >  
+                          <?php
+                            if (isset($_SESSION['is_auth'])) {
+                                echo "Welcome " . $_SESSION['name'];
+                            }
+                            ?> 
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a href="<?php echo URL . "sprofile/logout"; ?>">
+                            <?php if (isset($_SESSION['is_auth'])) {
+                                echo "Logout"; 
+                                }
+                            ?>
+                        </a>
+                    </li>
+
                 </ul>
             </div>
         </nav>
@@ -63,19 +103,28 @@
                         <h4 class="modal-title">Sign In</h4>
                     </div>
                     <div class="modal-body">
+                      <form id="sign_in_form" class="sign-in-form" action="<?php echo URL . "sprofile/index"; ?>" method="post" enctype="multipart/form-data" data-toggle="validator">
+                        <input type="hidden" name="fname" >
+                        <div class="form-group has-feedback">
+                          <input type="email" class="form-control" name="email" placeholder="You@Provider.com" data-error="Please provide a valid E-mail!" required >
+                          <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                          <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group has-feedback">
+                          <input type="password" class="form-control" name="password" placeholder="Password" data-minlength="8"required >
+                          <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                          <div class="help-block with-errors"></div>
+                        </div>
+                        <input type="hidden" name="url" value="<?php echo str_replace("&","?",explode('=', $_SERVER['QUERY_STRING'], 2)[1]);?>" >
+                        <input type="submit" name="sign-in" class="modal-submit" value="Sign In" >
+                      </form>
                     </div>
-                    <form class="sign-in-form"  action="<?php echo URL . "sprofile/index"; ?>" method="post" enctype="multipart/form-data">
-                        <input type="hidden" name="fname">                
-                        <input type="email" name="email" placeholder="You@Provider.com">
-                        <input type="password" name="password" placeholder="Password">
-                        <input type="submit" name="sign-in" class="modal-submit" value="Sign In">
-                    </form>
                     <div class="modal-footer">
                         Don't have an account? <a href="#" type="button" data-toggle="modal" data-target="#sign-up-modal" data-dismiss="modal">Register</a>
                     </div>
                 </div>
             </div>
-        </div>		
+        </div>
         <!-- Modal for Sign Up -->
         <div id="sign-up-modal" class="modal fade" role="dialog">
             <div class="modal-dialog">
@@ -85,27 +134,51 @@
                         <h4 class="modal-title">Sign Up</h4>
                     </div>
                     <div class="modal-body">
+                      <form class="sign-up-form" action="<?php echo URL . "sprofile/index"; ?>" method="post" enctype="multipart/form-data" data-toggle="validator" >
+                        <div class="form-group has-feedback">
+                          <label for="input_name">Full Name:</label>
+                          <input type="text" id="input_name"class="form-control" name="fname" placeholder="" required >
+                          <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                          <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group has-feedback">
+                          <label for="input_email">Email:</label>
+                          <input type="email" id="input_email"class="form-control" name="email" placeholder="You@Provider.com" data-error="Please provide a valid E-mail!" required >
+                          <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                          <div class="help-block with-errors"></div>
+                        </div>
+                        <div class="form-group has-feedback">
+                          <label for="input_password">Password:</label>
+                          <input type="password" id="input_password" class="form-control" name="password" data-minlength="8" required >
+                          <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                          <div class="help-block with-errors">Must have at least 8 characters</div>
+                        </div>
+                        <div class="form-group has-feedback">
+                          <label for="input_verify_password">Verify Password:</label>
+                          <input type="password" id="input_verify_password" class="form-control" name="verifyPassword" data-match="#input_password" data-match-error="Passwords do not match" required >
+                          <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+                          <div class="form-group has-feedback"></div>
+                        </div>
+                        <div class="form-group">
+                          <p>I am a:</p>
+                          <label class="radio-inline">
+                            <input type="radio" class="form-control" name="student" value="student"> 
+                            <p>Student looking to rent</p>
+                          </label>
+                          <br>
+                          <label class="radio-inline">
+                            <input type="radio" class="form-control" name="landLord" value="landlord">
+                            <p>Landlord looking to lease</p>
+                          </label>
+                        </div>
+                        <input type="hidden" name="url" value="<?php echo str_replace("&","?",explode('=', $_SERVER['QUERY_STRING'], 2)[1]);?>" >
+                        <input type="submit" class="form-control" name="register" class="modal-submit" value="Register">
+                      </form>
                     </div>
-                    <form class="sign-up-form" action="<?php echo URL . "sprofile/index"; ?>" method="post" enctype="multipart/form-data">
-                        <p>Full Name:</p>
-                        <input type="text" class="fnameclas" name="fname" placeholder="Ehsan Ali">
-                        <p>Email:</p> 
-                        <input type="email" name="email" placeholder="You@Provider.com">
-                        Password:
-                        <p class="disclaimer">Must have at least 8 characters</p>
-                        <input type="password" name="password"/>
-                        Verify Password:
-                        <input type="password" name="verifyPassword"/>
-                        <br><br>
-                        <input type="radio" name="student" value="student"/> I am a student who wants to rent.
-                        <br><br>
-                        <input type="radio" name="landLord" value="landlord"/> I am a landlord who wants to post.
-                        <br><br>
-                        <input type="submit" name="register" class="modal-submit" value="Register">
-                    </form>
                     <div class="modal-footer">
                         Already have an account? <a href="#" type="button" data-toggle="modal" data-target="#sign-in-modal" data-dismiss="modal">Sign In</a>
                     </div>
                 </div>
             </div>
+        </div>
     </header>
