866b3db047cb445bb1fd1accea15d992dc50aaf0
Darin <darinevanow@gmail.com>
2016-12-06 20:20:49 -0800

    Removed excess listing information, implemented basic (and horrible) carousel

--- application/view/viewRentalListing/index.php
+++ application/view/viewRentalListing/index.php
@@ -7,78 +7,63 @@
         <?php
     }
     ?>
-    <h2>Rental Space Details</h2>
-    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
-        <div class="panel-heading">Type:</div>
-        <div class="panel-body"><?php echo $rental_listing_type ?></div>
-    </div>
-    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
-        <div class="panel-heading">Description:</div>
-        <div class="panel-body"><?php echo $rental_listing_description ?></div>
-    </div>
-    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
-        <div class="panel-heading">Address:</div>
-        <div class="panel-body"><?php echo $rental_listing_address ?></div>
-    </div>
-    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
-        <div class="panel-heading">Price:</div>
-        <div class="panel-body">$ <?php echo $rental_listing_price ?></div>
-    </div>
-    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
-        <div class="panel-heading">Occupants:</div>
-        <div class="panel-body"><?php echo $rental_listing_occupants ?></div>
-    </div>
-    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
-        <div class="panel-heading">Pictures: Click on images for slide show</div>
-        <div class="panel-body">
-            <div class="slideshow-container">
-                <?php
-                foreach ($image_results as $image) {
-                    ?>
-                    <img class="myImg" width="100px" height="100px" src="./../uploads/<?php echo $image["image_name"] ?>">
-                <?php }
-                ?>
-            </div>
-        </div>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
         <?php
-        if (isset($_SESSION['is_auth'])) {
-            ?>
-            <a href="#" class="btn btn-default btn-lg pull-right" role="button" onclick="confirmDeleteModal('<?php echo $this->rental_ids[$i] ?>')">Rent</a>
-            <?php
-        }else {
-            ?>
-            <a href="#" class="btn btn-default btn-lg pull-right" type="button" data-toggle="modal" data-target="#sign-in-modal">Rent</a>
-            <?php
-        }
+        for ($i = 0; $i < count($image_results); $i++) {
         ?>
-        <div id="successMessage" style="font-size:20px;color:green;font-weight:bold;"></div>
-    </div>
-    <div id="myModal" class="image-modal">
-        <div class="slideshow-container">
+            <li data-target="#myCarousel" data-slide-to="<?php echo $i ?>"
+                <?php if($i == 0) { ?>
+                    class="active"
+                <?php } ?>>
+            </li>
+        <?php }
+        ?>
+      </ol>
 
-            <?php
-            foreach ($image_results as $image) {
-                ?>
-                <div class="mySlides image-fade">
-                    <img width="100%" height="600px" class="myImg"  src="./../uploads/<?php echo $image["image_name"] ?>">
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+          <?php for ($i = 0; $i < count($image_results); $i++) { ?>
+                <div class="item <?php if($i == 0) { ?> active" <?php } ?>">
+                    <img class="carousel-image" src="./../uploads/<?php echo $image_results[$i]["image_name"] ?>" alt="Chania">
                 </div>
-            <?php }
-            ?>
-            <span class="image-close">×</span>
-            <a class="prev" onclick="plusSlides(-1)">❮</a>
-            <a class="next" onclick="plusSlides(1)">❯</a>
-            <br>
-            <?php $count = 1 ?>
-            <div style="text-align:center">
-                <?php
-                foreach ($image_results as $image) {
-                    ?>
-                    <span class="dot" onclick="currentSlide(<?php echo $count ?>)"></span>
-                    <?php $count += 1 ?>
-                <?php }
+          <?php } ?>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <div class="panel panel-primary col-xs-18 col-sm-6 col-md-7">
+        <div class="panel-heading">Information about this Listing</div>
+        <ul class="list-group">
+          <li class="list-group-item">Room Type: <?php echo $rental_listing_type ?></li>
+          <li class="list-group-item">Description: <?php echo $rental_listing_description ?></li>
+          <li class="list-group-item">Address: <?php echo $rental_listing_address ?></li>
+          <li class="list-group-item">Price: $<?php echo $rental_listing_price ?></li>
+          <li class="list-group-item">Occupants<?php echo $rental_listing_occupants ?></li>
+          <li class="list-group-item">Pets Allowed: <?php echo ($rental_listing_pets ? "Yes" : "No") ?></li>
+          <li class="list-group-item">
+          <?php
+            if (isset($_SESSION['is_auth'])) {
                 ?>
-            </div>
-        </div>
+                <a href="#" class="btn btn-default btn-lg pull-right" role="button" onclick="confirmDeleteModal('<?php echo $this->rental_ids[$i] ?>')">Rent</a>
+                <?php
+            }else {
+                ?>
+                <a href="#" class="btn btn-default btn-lg pull-right" type="button" data-toggle="modal" data-target="#sign-in-modal">Rent</a>
+                <?php
+            }
+            ?>
+          </li>
+        </ul>
     </div>
     <!-- google map will be shown here -->
     <div id="gmap_canvas" style=" height:400px;">Loading map...</div>
