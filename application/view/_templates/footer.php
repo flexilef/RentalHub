b23f4632248965ba47a6b035332208f624358b1d
Muhammad Osama Nasir <muhammad-osama.nasir@informatik.hs-fulda.de>
2016-12-10 15:30:10 +0000

    Update footer.php

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,25 +1,182 @@
 </body>
-    <footer>
-        <!-- backlink to repo on GitHub, and affiliate link to Rackspace if you want to support the project -->
-        <div class="footer">
-            Created with the joint effort of SFSU and Fulda
-        </div>
+<footer>
+    <!-- backlink to repo on GitHub, and affiliate link to Rackspace if you want to support the project -->
+    <div class="footer">
+        Created with the joint effort of SFSU and Fulda
+    </div>
 
-        <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
-        <script>
-            var url = "<?php echo URL; ?>";
-        </script>
+    <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+    <script>
+        var url = "<?php echo URL; ?>";
+    </script>
 
-        <!-- Include Jquery from CDN -->
-        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <!-- Include Jquery from CDN -->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 
-        <!-- our JavaScript -->
-        <script src="<?php echo URL; ?>js/application.js"></script>
+    <!-- our JavaScript -->
+    <script src="<?php echo URL; ?>js/application.js"></script>
 
-        <!-- include less compiler -->
-        <script src="<?php echo URL; ?>js/less.min.js"></script>
+    <!-- include less compiler -->
+    <script src="<?php echo URL; ?>js/less.min.js"></script>
 
-        <!-- Include Bootstrap from CDN -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </footer>
+    <!-- Include Bootstrap from CDN -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.js"></script>
+
+    <script>
+        $(document).ready(function () {
+
+            $('.delete_product').click(function (e) {
+
+                e.preventDefault();
+
+                var pid = $(this).attr('data-id');
+                var parent = $(this).parent("td").parent("tr");
+
+                bootbox.dialog({
+                    message: "Are you sure you want to Delete ?",
+                    title: "<i class='glyphicon glyphicon-trash'></i> Delete !",
+                    buttons: {
+                        success: {
+                            label: "No",
+                            className: "btn-success",
+                            callback: function () {
+                                $('.bootbox').modal('hide');
+                            }
+                        },
+                        danger: {
+                            label: "Delete!",
+                            className: "btn-danger",
+                            callback: function () {
+
+                                /*
+                                 
+                                 using $.ajax();
+                                 
+                                 $.ajax({
+                                 
+                                 type: 'POST',
+                                 url: 'delete.php',
+                                 data: 'delete='+pid
+                                 
+                                 })
+                                 .done(function(response){
+                                 
+                                 bootbox.alert(response);
+                                 parent.fadeOut('slow');
+                                 
+                                 })
+                                 .fail(function(){
+                                 
+                                 bootbox.alert('Something Went Wrog ....');
+                                 
+                                 })
+                                 */
+
+                                $.post('delete', {'delete': pid})
+                                        .done(function (response) {
+                                            bootbox.alert(response);
+                                            parent.fadeOut('slow');
+                                        })
+                                        .fail(function () {
+                                            bootbox.alert('Something Went Wrog ....');
+                                        })
+
+
+                            }
+                        }
+                    }
+                });
+
+
+            });
+
+            $('#send_email').click(function (e) {
+
+                e.preventDefault();
+
+                var pid = $(this).attr('data-id');
+                var parent = $(this).parent("td").parent("tr");
+
+                bootbox.dialog({
+                    message: "Your contact details will be shared with the owner",
+                    title: "<i class='glyphicon glyphicon-envelope'></i> Send Email Now!",
+                    buttons: {
+                        success: {
+                            label: "No",
+                            className: "btn-success",
+                            callback: function () {
+                                $('.bootbox').modal('hide');
+                            }
+                        },
+                        danger: {
+                            label: "Yes!",
+                            className: "btn-success",
+                            callback: function () {
+
+                                $.post('sendEmail', {'email': pid})
+                                        .done(function (response) {
+                                            bootbox.alert(response);
+                                            parent.fadeOut('slow');
+                                        })
+                                        .fail(function () {
+                                            bootbox.alert('Something Went Wrog ....');
+                                        })
+
+
+                            }
+                        }
+                    }
+                });
+
+
+            });
+
+            $('#send_email_again').click(function (e) {
+
+                e.preventDefault();
+
+                var pid = $(this).attr('data-id');
+                var parent = $(this).parent("td").parent("tr");
+
+                bootbox.dialog({
+                    message: "Your contact details will be shared with the owner",
+                    title: "<i class='glyphicon glyphicon-envelope'></i> Send Email Now!",
+                    buttons: {
+                        success: {
+                            label: "No",
+                            className: "btn-success",
+                            callback: function () {
+                                $('.bootbox').modal('hide');
+                            }
+                        },
+                        danger: {
+                            label: "Yes!",
+                            className: "btn-success",
+                            callback: function () {
+
+                                $.post('sendEmail', {'email': pid})
+                                        .done(function (response) {
+                                            bootbox.alert(response);
+                                            parent.fadeOut('slow');
+                                        })
+                                        .fail(function () {
+                                            bootbox.alert('Something Went Wrog ....');
+                                        })
+
+
+                            }
+                        }
+                    }
+                });
+
+
+            });
+
+        });
+    </script>
+</footer>
 </html>
