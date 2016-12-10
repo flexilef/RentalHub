676b943071f8126e154891192b9e08e424a86fc4
Muhammad Osama Nasir <muhammad-osama.nasir@informatik.hs-fulda.de>
2016-12-10 15:30:37 +0000

    Update index.php

--- application/view/profilePage/index.php
+++ application/view/profilePage/index.php
@@ -1,166 +1,146 @@
 <hr>
 <div class="container">
-  <div class="row">
-      <div class="col-sm-10"><h1>John Doe</h1></div>
+    <div class="row">
+        <div class="col-sm-10"><h1><?php echo $_SESSION['name'] ?></h1></div>
     </div>
     <div class="row">
-      <div class="col-sm-3"><!--left col-->
+        <div class="col-sm-3"><!--left col-->
 
-          <ul class="list-group">
-            <li class="list-group-item text-muted">Profile</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Joined</strong></span> 11/14/2016</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>User Type</strong></span> Landlord</li>
-            <li class="list-group-item text-right"><span class="pull-left"><strong>Email</strong></span> jd@host.com</li>
-          </ul>
+            <ul class="list-group">
+                <li class="list-group-item text-muted">Profile</li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong>Joined</strong></span><?php echo $_SESSION['createdDate'] ?></li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong>User Type</strong></span> <?php echo $_SESSION['userTypeDescription'] ?></li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong>Email</strong></span> <?php echo $_SESSION['email'] ?></li>
+            </ul>
 
         </div><!--/col-3-->
-      <div class="col-sm-9">
+        <div class="col-sm-9">
 
-          <ul class="nav nav-tabs" id="myTab">
-            <li class="active"><a href="#listings" data-toggle="tab">Listings</a></li>
-            <li><a href="#settings" data-toggle="tab">Settings</a></li>
-          </ul>
+            <ul class="nav nav-tabs" id="myTab">
+                <li class="active"><a href="#listings" data-toggle="tab">Property Listings</a></li>
+                <li><a href="#settings" data-toggle="tab">User Setting</a></li>
+            </ul>
 
-          <div class="tab-content">
-            <div class="tab-pane active" id="listings">
-              <div class="table-responsive">
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>Title</th>
-                      <th>Description</th>
-                      <th>Address</th>
-                      <th>Price</th>
-                      <th>Edit </th>
-                      <th>Delete </th>
-                    </tr>
-                  </thead>
-                  <tbody id="items">
-                    <tr>
-                      <td>1</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-                    </tr>
-                    <tr>
-                      <td>5</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td>Table cell</td>
-                      <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-                    </tr>
-                  </tbody>
-                </table>
-                <hr>
-              </div><!--/table-resp-->
-             </div><!--/tab-pane-->
-             <div class="tab-pane" id="settings">
+            <div class="tab-content">
+                <div class="tab-pane active" id="listings">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Address</th>
+                                    <th>Price</th>
+                                    <th>Delete </th>
+                                </tr>
+                            </thead>
+                            <tbody id="items">
 
-                  <hr>
-                  <form class="form" action="##" method="post" id="registrationForm">
-                      <div class="form-group">
+                                <?php
+                                if (!empty($this->posted_rental_properties)) {
+                                       foreach ( $this->posted_rental_properties as $value) {
+                                        ?>  
+                                        <tr>
+                                            <td><?php echo $value['ID']; ?></td>
+                                            <td><?php echo $value['TITLE']; ?></td>
+                                            <td><?php echo $value['DESCRIPTION']; ?></td>
+                                            <td><?php echo $value['ADDRESS']; ?></td>
+                                            <td><?php echo $value['PRICE']; ?></td>
+                                            <td>
+                                                <a class="delete_product" data-id="<?php echo $value['ID']; ?>" href="javascript:void(0)">
+                                                    <i class="glyphicon glyphicon-trash"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
 
-                          <div class="col-xs-6">
-                              <label for="first_name"><h4>First name</h4></label>
-                              <input type="text" class="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
+                                        <?php
+                                    }
+                                } else {
+                                    ?>  <tr>
+                                        <td>No result found</td>
+                                    </tr>
+                                    <?php
+                                }
+                                ?>    
+                            </tbody>
+                        </table>
+                        <hr>
+                    </div><!--/table-resp-->
+                </div><!--/tab-pane-->
+                <div class="tab-pane" id="settings">
 
-                          <div class="col-xs-6">
-                            <label for="last_name"><h4>Last name</h4></label>
-                              <input type="text" class="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any.">
-                          </div>
-                      </div>
+                    <hr>
+                    <form id="registrationForm" class="form" action="<?php echo URL . "profile/index"; ?>" method="post" enctype="multipart/form-data" >
+                        <div class="form-group">
 
-                      <div class="form-group">
+                            <div class="col-xs-6">
+                                <label for="first_name"><h4>First name</h4></label>
+                                <input type="text" class="form-control"  name="firstName" id="first_name" placeholder="first name" title="enter your first name if any.">
+                            </div>
+                        </div>
+                        <div class="form-group">
 
-                          <div class="col-xs-6">
-                              <label for="phone"><h4>Phone</h4></label>
-                              <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
-                          </div>
-                      </div>
+                            <div class="col-xs-6">
+                                <label for="last_name"><h4>Last name</h4></label>
+                                <input type="text" class="form-control" name="lastName" id="last_name" placeholder="last name" title="enter your last name if any.">
+                            </div>
+                        </div>
 
-                      <div class="form-group">
-                          <div class="col-xs-6">
-                             <label for="mobile"><h4>Mobile</h4></label>
-                              <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
-                          </div>
-                      </div>
-                      <div class="form-group">
+                        <div class="form-group">
 
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Email</h4></label>
-                              <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
-                          </div>
-                      </div>
-                      <div class="form-group">
+                            <div class="col-xs-6">
+                                <label for="phone"><h4>Phone</h4></label>
+                                <input type="text" class="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any.">
+                            </div>
+                        </div>
 
-                          <div class="col-xs-6">
-                              <label for="email"><h4>Location</h4></label>
-                              <input type="email" class="form-control" id="location" placeholder="somewhere" title="enter a location">
-                          </div>
-                      </div>
-                      <div class="form-group">
+                        <div class="form-group">
+                            <div class="col-xs-6">
+                                <label for="mobile"><h4>Mobile</h4></label>
+                                <input type="text" class="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any.">
+                            </div>
+                        </div>
+                        <div class="form-group">
 
-                          <div class="col-xs-6">
-                              <label for="password"><h4>Password</h4></label>
-                              <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
-                          </div>
-                      </div>
-                      <div class="form-group">
+                            <div class="col-xs-6">
+                                <label for="email"><h4>Email</h4></label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email.">
+                            </div>
+                        </div>
+                        <div class="form-group">
 
-                          <div class="col-xs-6">
-                            <label for="password2"><h4>Verify</h4></label>
-                              <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
-                          </div>
-                      </div>
-                      <div class="form-group">
-                           <div class="col-xs-12">
+                            <div class="col-xs-6">
+                                <label for="email"><h4>Location</h4></label>
+                                <input type="text" class="form-control" name="address" id="location" placeholder="somewhere" title="enter a location">
+                            </div>
+                        </div>
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="password"><h4>Password</h4></label>
+                                <input type="password" class="form-control" name="password" id="password" placeholder="password" title="enter your password.">
+                            </div>
+                        </div>
+                        <div class="form-group">
+
+                            <div class="col-xs-6">
+                                <label for="password2"><h4>Verify</h4></label>
+                                <input type="password" class="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2.">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-xs-12">
                                 <br>
-                                <button class="btn btn-lg" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
+                                <button class="btn btn-lg" name="update" type="submit"><i class="glyphicon glyphicon-ok-sign"></i> Save</button>
                                 <button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>
                             </div>
-                      </div>
-                </form>
-              </div>
+                        </div>
+                    </form>
+                </div>
+            </div><!--/tab-pane-->
+        </div><!--/tab-content-->
 
-              </div><!--/tab-pane-->
-          </div><!--/tab-content-->
+    </div><!--/col-9-->
+</div><!--/row-->
 
-        </div><!--/col-9-->
-    </div><!--/row-->
-</div>
