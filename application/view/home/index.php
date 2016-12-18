5aa25c51db7133959f279abf1d8660811f74409e
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-12-17 21:30:16 -0800

    updated the search place holder text

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -50,7 +50,7 @@
 								<?php
 									}else {
 									?>
-                                <a href="#" class="btn btn-success btn-lg pull-right" type="button" data-toggle="modal" data-target="#sign-in-modal">Contact Landlord</a>
+                                <a href="#" class="btn btn-success btn-lg pull-right" type="button" data-toggle="modal" data-target="#sign-in-modal">Contact</a>
                                 <?php
 									}
 									?>
