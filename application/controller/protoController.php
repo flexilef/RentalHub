aa87c824616983750fc4280951b221c1a6770504
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-12-20 07:30:27 -0800

    checking for large image size upload in posting listing

--- application/controller/protoController.php
+++ application/controller/protoController.php
@@ -16,6 +16,9 @@ class ProtoController extends Controller {
     }
 
     public function index() {
+        if (isset($_GET['error'])) {
+            echo '<div class="alert alert-danger" role="alert"><strong>Error!</strong>Upload Image size should be less than 2 MB.</div>';
+        }
         require APP . 'view/_templates/header.php';
         require APP . "view/protoView/index.php";
         require APP . 'view/_templates/footer.php';
@@ -39,20 +42,26 @@ class ProtoController extends Controller {
                     $image = stripslashes($_FILES['images']['name'][$name]);
                     $image = rand(1000, 100000) . "-" . $_FILES['images']['name'][$name];
                     $image_loc = $_FILES['images']['tmp_name'][$name];
-                    $image_size = $_FILES['images']['size'][$name];
                     $image_type = $_FILES['images']['type'][$name];
                     $folder = APP . "../public/uploads/";
+                    $image_size = filesize($image_loc);
 
                     // new file size in KB
                     $new_image_size = $image_size / 1024;
+
+                    if (!$image_size || $new_image_size > 1500) {
+                        header('Location:' . URL . 'protoController/index?error=largeImage');
+                    }
 
                     // make file name in lower case
                     $new_image_name = strtolower($image);
 
                     $final_image = str_replace(' ', '-', $new_image_name);
 
-                    move_uploaded_file($_FILES['images']['tmp_name'][$name], $folder . $final_image); // Move the uploaded file to the desired folder
-                    $this->image_model->uploadImage($final_image, $image_type, $new_image_size, $rental_listing_id);
+                    if (move_uploaded_file($_FILES['images']['tmp_name'][$name], $folder . $final_image)) {
+                        // Move the uploaded file to the desired folder
+                        $this->image_model->uploadImage($final_image, $image_type, $new_image_size, $rental_listing_id);
+                    }
                 }
             } else {
                 //have a default image for postings that don't have images. Else, make it strict to post with pics
